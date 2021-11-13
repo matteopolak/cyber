d291ff1f278f9588c6dc6e05749405b62f251035
matteopolak <jackson.bourne.1993@gmail.com>
2021-11-12 23:54:38 -0500

    ubu20, ubu18, ubu16 CIS-compliant hardening scripts

---
+++ src/external/linux/ubu18/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install dependencies
+apt install ansible -y
+
+mkdir /etc/ansible
+cd /etc/ansible
+
+echo '- src: https://github.com/matteopolak/ubuntu1804_cis.git' >> /etc/ansible/requirements.yml;
+
+ansible-galaxy install -p roles -r /etc/ansible/requirements.yml
+
+cat > /etc/ansible/harden.yml << EOF
+- name: Harden Server
+  hosts: localhost
+  connection: local
+  become: yes
+
+  roles:
+    - ubuntu1804_cis
+EOF
+
+ansible-playbook /etc/ansible/harden.yml
