8128a8cfaf5fb76c586367f046f79573f1ce553a
matteopolak <jackson.bourne.1993@gmail.com>
2021-11-20 16:36:35 -0500

    file diff checking & filtering diff result

--- external/windows/winser2016/setup.ps1
+++ external/windows/winser2016/setup.ps1
@@ -1,10 +1,12 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 Install-Module AuditPolicyDSC -Force
 Install-Module ComputerManagementDSC -Force
 Install-Module SecurityPolicyDSC -Force
 
 ./WindowsServer2016.ps1
 
-Set-NetConnectionProfile -NetworkCategory Private -Force
+Set-NetConnectionProfile -NetworkCategory Private
 Set-Item -Path WSMan:\localhost\MaxEnvelopeSizeKb -Value 2048 -Force
 
 winrm quickconfig
--- internal/windows/find-winser2019.bat
+++ internal/windows/find-winser2019.bat
@@ -1,8 +1,8 @@
-:: create a new win10.tar.gz with the following commands:
+:: create a new winser2019.tar.gz with the following commands:
 ::
 :: where /R \ * > default.txt
 :: sort default.txt /o default.txt
-:: tar -czf win10.tar.gz default.txt
+:: tar -czf winser2019.tar.gz default.txt
 
 :: pipe all file names to `tree.txt`
 where /R \ * > tree.txt
@@ -11,15 +11,15 @@ where /R \ * > tree.txt
 :: this uses bash brace expansion to avoid repeating filename
 sort tree.txt /o tree.txt
 
-:: unzip `win10.tar.gz`
-tar -xzf win10.tar.gz
+:: unzip `winser2019.tar.gz`
+tar -xzf winser2019.tar.gz
 
 :: `default.txt` contains base files
 :: -13 = print lines only present in second file
 "./bin/comm.exe" -13 default.txt tree.txt > diff.txt
 
 :: `diff.txt` contains all file paths and files that are not
-:: present in a default installation of Windows 10
+:: present in a default installation of Windows Server 2019
 
 :: filter out the `diff.txt` file to remove useless data
 ./filter-diff.bat
