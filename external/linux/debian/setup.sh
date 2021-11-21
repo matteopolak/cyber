d6fa06118945cebe4b43c78def351e7e69d4399a
matteopolak <jackson.bourne.1993@gmail.com>
2021-11-20 19:35:58 -0500

    use 'source' instead of 'bash' to call debian script

--- external/linux/debian/setup.sh
+++ external/linux/debian/setup.sh
@@ -12,7 +12,7 @@ cd debian-cis
 cp debian/default /etc/default/cis-hardening
 sed -i "s#CIS_ROOT_DIR=.*#CIS_ROOT_DIR='$(pwd)'#" /etc/default/cis-hardening
 
-bash bin/hardening.sh --apply --allow-unsupported-distribution
+source bin/hardening.sh --apply --allow-unsupported-distribution
 
 mkdir /etc/ansible
 cd /etc/ansible
