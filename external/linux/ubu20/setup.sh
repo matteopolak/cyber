a75aa32d2b49d8193d4dd3207fe75bc14fac1b1c
matteopolak <jackson.bourne.1993@gmail.com>
2021-12-10 17:52:03 -0500

    add 3 hardening scripts for ubu20

--- external/linux/ubu20/setup.sh
+++ external/linux/ubu20/setup.sh
@@ -3,13 +3,20 @@
 # move to script directory
 cd "$(dirname "$0")"
 
+# add ansible repo so we can use version 2.9+
+add-apt-repository --yes --update ppa:ansible/ansible
+
 # Install dependencies
 apt install ansible git -y
 
 mkdir /etc/ansible
 cd /etc/ansible
 
-echo '- src: https://github.com/matteopolak/ubuntu2004_cis.git' >> /etc/ansible/requirements.yml;
+echo << EOF
+- src: https://github.com/matteopolak/ubuntu2004_cis.git
+- src: https://github.com/alivx/CIS-Ubuntu-20.04-Ansible
+- src: https://github.com/ansible-lockdown/UBUNTU20-CIS
+EOF >> /etc/ansible/requirements.yml;
 
 ansible-galaxy install -p roles -r /etc/ansible/requirements.yml
 
@@ -21,6 +28,23 @@ cat > /etc/ansible/harden.yml << EOF
 
   roles:
     - ubuntu2004_cis
+
+- name: Harden Server
+  hosts: localhost
+  connection: local
+  become: yes
+
+  roles:
+    - UBUNTU20-CIS
+
+- name: Harden Server
+  hosts: localhost
+  connection: local
+  become: yes
+	allowed_users: root cool
+
+  roles:
+    - CIS-Ubuntu-20.04-Ansible
 EOF
 
 ansible-playbook /etc/ansible/harden.yml
